2b34b80834e09ec9934299a1482ad0c607b57228
unknown <dimka.puchir1997@gmail.com>
2017-10-23 19:17:53 +0300

    My Class

--- Purse/Purse/ICard.cs
+++ Purse/Purse/ICard.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Purse
 {
-    public interface ICard
+    interface ICard
     {
         double Balance();
         double Income();
--- Purse/Purse/Program.cs
+++ Purse/Purse/Program.cs
@@ -11,20 +11,9 @@ namespace Purse
         static void Main(string[] args)
         {
             Purse purse = new Purse();
-            Credit_card c1 = new Credit_card("D.Pukhir", 4098);
-            c1.Balance();
-            c1.Card_name();
-            c1.Income();
-            c1.Outlay();
-            c1.Add_money(300);
-            c1.Get_cash(500);
-            purse.Add_card(c1);
-
             purse.Add_card(new Gold_card());
-            
+            purse.Add_card(new Credit_card());
             purse.Add_card(new Business_card());
-              
-            
         }
     }
 }
